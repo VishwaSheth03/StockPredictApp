ebd2f4b2740f8a2a201819aa4160e058801add08
Melody <melody.li@hubdoc.com>
2019-03-28 13:20:47 -0400

    Plot

--- stock_predict.py
+++ stock_predict.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import matplotlib.pyplot as plt
 
 API_KEY = '58SXCV92ZE1JUZ62'
 STOCK_NAME = 'AAPL'
@@ -7,7 +8,15 @@ STOCK_NAME = 'AAPL'
 r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + STOCK_NAME + '&outputsize=full&apikey=' + API_KEY)
 
 result = r.json()
-dataForAllDays = result['Time Series (Daily)']
+dataForAllDays = result['Time Series (Daily)'] 
+#     key: '2019-03-22' 
+#     value: {
+#     "1. open": "188.9500",
+#     "2. high": "189.5500",
+#     "3. low": "187.8200",
+#     "4. close": "188.4400",
+#     "5. volume": "7570388"
+# }
 dataForSingleDate = dataForAllDays['2019-03-22']
 opening = dataForSingleDate['1. open']
 high = dataForSingleDate['2. high']
@@ -20,8 +29,14 @@ close = dataForSingleDate['4. close']
 volume = dataForSingleDate['5. volume']
 y = opening*1000
 #print(y)
-for x,y in dataForAllDays.items(): 
-  print(y['1. open'])
 
+dates = []
+closing_price = []
+for x,y in dataForAllDays.items(): 
+  dates.append(x)
+  closing_price.append(float(y['4. close']))
+
+plt.plot(closing_price[5000:])
+plt.show()
 
 #TEST TEST
